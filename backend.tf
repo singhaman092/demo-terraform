0d5a6bc82a29cad6337be9a04f9a6b86e4b3abcd
aman <singhaman092@gmail.com>
2021-05-29 19:22:28 +0530

    added new backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-remote-aman-1"
+    bucket         = "tf-state-aman"
     key            = "backend.tfstate"
     region         = "us-east-1"    # Replace this with your DynamoDB table name!
-    dynamodb_table = "aws-tf-locks-table"
+    dynamodb_table = "tf-state-aman"
     encrypt        = true
   }
 }
