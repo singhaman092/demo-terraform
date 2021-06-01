99a0ed8c622c7ed01df1f2f3e47ffbe8406ee42c
singhaman092 <46333790+singhaman092@users.noreply.github.com>
2021-06-01 05:34:48 +0530

    removed existing infrastructure

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-aman-1"
+# resource "aws_s3_bucket" "b" {
+#   bucket = "my-tf-test-bucket-aman-1"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
 
-  provider = aws.new
-}
-resource "aws_s3_bucket" "c" {
-  bucket = "my-tf-test-bucket-aman-2"
+#   provider = aws.new
+# }
+# resource "aws_s3_bucket" "c" {
+#   bucket = "my-tf-test-bucket-aman-2"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
 
-  provider = aws.new
-}
-resource "aws_s3_bucket" "d" {
-  bucket = "my-tf-test-bucket-aman-3"
+#   provider = aws.new
+# }
+# resource "aws_s3_bucket" "d" {
+#   bucket = "my-tf-test-bucket-aman-3"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
 
-  provider = aws.new
-}
+#   provider = aws.new
+# }
