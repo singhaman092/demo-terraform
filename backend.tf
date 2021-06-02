668a397b937d8c42b9e3e969ccaf0fc0835a813d
singhaman092 <46333790+singhaman092@users.noreply.github.com>
2021-06-02 07:52:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "tf-state-aman"
+    bucket         = "aman-tf-backend-cf"
     key            = "backend.tfstate"
     region         = "us-east-1"    # Replace this with your DynamoDB table name!
-    dynamodb_table = "tf-state-aman"
+    dynamodb_table = "aman-tf-backend-cf"
     encrypt        = true
   }
 }
