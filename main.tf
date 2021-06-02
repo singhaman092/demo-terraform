b544165eab8b941f67af57e275aed4aa762c20d9
singhaman092 <46333790+singhaman092@users.noreply.github.com>
2021-06-02 06:52:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-aman-1"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-  provider = aws.new
+resource "aws_instance" "foo" {
+  ami           = "ami-0d5eff06f840b45e9" # us-west-2
+  instance_type = "t2.micro"
 }
+
+
+
+
+
+# resource "aws_s3_bucket" "b" {
+#   bucket = "my-tf-test-bucket-aman-1"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+
+#   provider = aws.new
+# }
 # resource "aws_s3_bucket" "c" {
 #   bucket = "my-tf-test-bucket-aman-2"
 
